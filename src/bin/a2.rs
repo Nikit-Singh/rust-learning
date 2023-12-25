aa1cd5bd3abf8d3ffde9561d1ce10aba2760988c
Nikit Singh <nikitskulhari@gmail.com>
2023-12-26 04:52:46 +0530

    solve a2 and a3

--- src/bin/a2.rs
+++ src/bin/a2.rs
@@ -8,4 +8,15 @@
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result
 
-fn main() {}
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+fn display_result() {
+    println!("{:?}", add(3, 4));
+}
+
+fn main() {
+    display_result();
+}
+
